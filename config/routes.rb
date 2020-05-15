c4fa61f5edc471c816b07c0f01def58abaaa764c
sabosaboten <deyakei@gmail.com>
2020-05-15 15:29:58 +0900

    導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root "schedules#index"
+  resources :schedules, only: [:new, :create, :edit, :update, :index, :destroy]
+  resources :users, only: [:edit, :update]
 end
+
