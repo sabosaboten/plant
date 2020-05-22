868c98ead281748c92c66ecd02df22713f9172f3
sabosaboten <deyakei@gmail.com>
2020-05-22 11:46:58 +0900

    同一マップ上に複数の場所記載

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -2,6 +2,11 @@ class ShopsController < ApplicationController
   def index
     @shop = Shop.new
     @shops = Shop.all
+    @hash = Gmaps4rails.build_markers(@shops) do |shop, marker|
+      marker.lat shop.latitude
+      marker.lng shop.longitude
+      marker.infowindow shop.name
+    end
   end
 
   def show
